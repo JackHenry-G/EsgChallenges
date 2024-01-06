8b65a9116fdf46d90b6ac45a10a88a794b6c27ca
JackHenry-G <Jack_HG@outlook.com>
2024-01-06 21:25:10 +0000

    More testing

--- customer-api-project/csvreader/src/test/java/com/esg/CsvProcesserTest.java
+++ customer-api-project/csvreader/src/test/java/com/esg/CsvProcesserTest.java
@@ -1,13 +1,54 @@
 package com.esg;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Test;
-
 public class CsvProcesserTest {
+
+    private CsvProcessor<Customer> csvProcessor;
+
+    @BeforeEach
+    public void BeforeAll() {
+        csvProcessor = new CsvProcessor<Customer>();
+    }
+
+    @Test
+    public void testParseCsvToJsonWithEmptyCsv() throws FileNotFoundException {
+        String expectedJson = "[]";
+
+        String actualJson = csvProcessor
+                .parseCsvToJson("src/test/resources/noCustomers.csv", Customer.class);
+
+        assertEquals(expectedJson, actualJson, "Empty CSV returned no JSON");
+    }
+
+    @Test
+    public void testParseCsvToJsonWithOneCustomerCsv() throws FileNotFoundException {
+        String expectedJson = "[{\"customerRef\":\"CUST001\",\"customerName\":\" John Smith\",\"addressLineOne\":\" 123 Oak Street\",\"addressLineTwo\":\" Suite 100\",\"town\":\" Springfield\",\"county\":\" Greene\",\"country\":\" USA\",\"postcode\":\" 65804\"}]";
+
+        String actualJson = csvProcessor
+                .parseCsvToJson("src/test/resources/oneCustomer.csv", Customer.class);
+
+        assertEquals(expectedJson, actualJson, "Empty CSV returned no JSON");
+    }
+
+    @Test
+    public void testParseCsvToJsonWithManyCustomersCsv() throws FileNotFoundException {
+        String expectedJson = "[{\"customerRef\":\"CUST001\",\"customerName\":\" John Smith\",\"addressLineOne\":\" 123 Oak Street\",\"addressLineTwo\":\" Suite 100\",\"town\":\" Springfield\",\"county\":\" Greene\",\"country\":\" USA\",\"postcode\":\" 65804\"},{\"customerRef\":\"CUST002\",\"customerName\":\" Jane Doe\",\"addressLineOne\":\" 456 Maple Avenue\",\"addressLineTwo\":\" Suite 200\",\"town\":\" Lakeside\",\"county\":\" Lake County\",\"country\":\" USA\",\"postcode\":\" 60073\"}]";
+
+        String actualJson = csvProcessor
+                .parseCsvToJson("src/test/resources/manyCustomers.csv", Customer.class);
+
+        assertEquals(expectedJson, actualJson, "Empty CSV returned no JSON");
+    }
 
     @Test
     public void testConvertToJson() {
@@ -21,6 +62,6 @@ public class CsvProcesserTest {
 
         CsvProcessor<Customer> csvProcessor = new CsvProcessor<Customer>();
 
-        assertEquals(expectedJson, csvProcessor.convertToJson(customerDetails));
+        assertEquals(expectedJson, csvProcessor.convertToJson(customerDetails), "Customers JSON received succesfullly");
     }
 }
